0720f70e81f4d70a7b6a48d2ea0119aee4771909
farrelac <carasiola46@gmail.com>
2025-05-05 17:16:48 +0700

    hi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        Paginator::useBootstrapFive();
+            URL::forceScheme('https');
     }
 }
