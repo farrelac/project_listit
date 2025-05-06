e0de065eb875bfaa4d6512d51b0ce0c0d77261b5
farrelac <carasiola46@gmail.com>
2025-05-06 15:56:39 +0700

    hfdsd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
         Paginator::useBootstrapFive();
     }
 }
